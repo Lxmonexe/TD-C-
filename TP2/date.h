c8ae78280654ed221a4856a16df725dd4af94155
Lxmonexe <93253891+Lxmonexe@users.noreply.github.com>
2023-12-07 08:27:56 +0100

    Ajout de la classe Date

---
+++ TP2/date.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+class Date
+{
+private:
+    int _year;
+    int _month;
+    int _day;
+public:
+    Date(int year=1, int month=1, int day=1);
+    void validDate();
+    void afficher();
+};
